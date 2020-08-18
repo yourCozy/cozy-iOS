57f46ed16bfd2e2384cbddc8c232c401aca7ad45
Choieunji0114 <cheunji8209@naver.com>
2020-08-18 21:31:08 +0900

    add: project setting
    프로젝트 초기 설정 : swiftlint, foldering

---
+++ cozy/cozy/Source/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  cozy
+//
+//  Created by 최은지 on 2020/08/18.
+//  Copyright © 2020 최은지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
