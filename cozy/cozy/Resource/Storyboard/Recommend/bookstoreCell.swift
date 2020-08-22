ac7d67dda7ce036b26a3a2e0d5801fb8fd516e1c
Choieunji0114 <cheunji8209@naver.com>
2020-08-23 02:33:42 +0900

    add: recommend tab tableview

---
+++ cozy/cozy/Resource/Storyboard/Recommend/bookstoreCell.swift
@@ -0,0 +1,26 @@
+//
+//  bookstoreCell.swift
+//  cozy
+//
+//  Created by 최은지 on 2020/08/23.
+//  Copyright © 2020 최은지. All rights reserved.
+//
+
+import UIKit
+
+class bookstoreCell: UITableViewCell {
+
+    @IBOutlet weak var bookstoreImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- cozy/cozy/Source/ViewControllers/Recommend/RecommendVC.swift
+++ cozy/cozy/Source/ViewControllers/Recommend/RecommendVC.swift
@@ -9,9 +9,57 @@
 import UIKit
 
 class RecommendVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    private let cellIdentifier1: String = "recommendCell"
+    private let cellIdentifier2: String = "bookstoreCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if let index = self.tableView.indexPathForSelectedRow {
+            self.tableView.deselectRow(at: index, animated: true)
+        }
+    }
+}
+
+extension RecommendVC: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return 5
+        }
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 99
+        } else {
+            return 342
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier1) as! recommendCell
+            cell.recommendLabel.text = "추천웅앵"
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier2) as! bookstoreCell
+            cell.bookstoreImageView.image = UIImage(named: "image1")
+            return cell
+        }
     }
 
 }
