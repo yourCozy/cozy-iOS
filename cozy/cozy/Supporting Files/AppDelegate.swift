133a5712e74d95c3b41fd534886e58aa0af92723
Choieunji0114 <cheunji8209@naver.com>
2020-08-22 22:22:02 +0900

    add: font, tab setting

--- cozy/cozy/Supporting Files/AppDelegate.swift
+++ cozy/cozy/Supporting Files/AppDelegate.swift
@@ -13,7 +13,8 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // tab bar tint color setting
+        UITabBar.appearance().tintColor = UIColor.mango
         return true
     }
 
