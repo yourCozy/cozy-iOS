fe28edc0110d3d3c7b08a6018ed73803558ded1e
Choieunji0114 <cheunji8209@naver.com>
2020-08-30 03:38:42 +0900

    fix: DetailCell1

--- cozy/cozy/Source/ViewControllers/Recommend/Cell/detailCell1.swift
+++ cozy/cozy/Source/ViewControllers/Recommend/Cell/detailCell1.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class detailCell1: UITableViewCell {
 
+    @IBOutlet weak var bookstoreImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    @IBOutlet weak var callButton1: UIButton!
+    @IBOutlet weak var callButton2: UIButton!
+    @IBOutlet weak var bookmarkButton1: UIButton!
+    @IBOutlet weak var bookmarkButton2: UIButton!
+    @IBOutlet weak var roadButton1: UIButton!
+    @IBOutlet weak var roadButton2: UIButton!
+
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var restLabel: UILabel!
+    @IBOutlet weak var homeLabel: UILabel!
+
+    @IBOutlet weak var bookButton: UIButton!
+    @IBOutlet weak var activityButton: UIButton!
+
+    @IBOutlet weak var bookUnderline: UIView!
+    @IBOutlet weak var activityUnderline: UIView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- cozy/cozy/Source/ViewControllers/Recommend/VC/BookDetailVC.swift
+++ cozy/cozy/Source/ViewControllers/Recommend/VC/BookDetailVC.swift
@@ -37,7 +37,7 @@ extension BookDetailVC: UITableViewDelegate, UITableViewDataSource {
         if section == 0 {
             return 1
         } else {
-            return 10
+            return 1
         }
     }
 
@@ -52,6 +52,30 @@ extension BookDetailVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: detailIdentifier1) as! detailCell1
+
+            cell.bookstoreImageView.image = UIImage(named: "image1")
+
+            cell.descriptionLabel.numberOfLines = 2
+            let style = NSMutableParagraphStyle()
+            style.lineSpacing = 5.0
+            let descripText = NSAttributedString(string: "우연히 만난 옛 친구같은 따뜻한 이야기가 머무는 책방을 꿈꿉니다. 이것은 무의미한 텍스트 입니다.")
+            let descripAttr = NSMutableAttributedString()
+            descripAttr.append(descripText)
+            descripAttr.addAttributes([.paragraphStyle: style], range: NSRange(location: 0, length: descripAttr.length))
+            cell.descriptionLabel.attributedText = descripAttr
+
+            cell.locationLabel.text = "서울특별시 용산구 한강대로 102길 57"
+            cell.timeLabel.text = "매일 13:00 ~ 19:00"
+
+            cell.restLabel.numberOfLines = 2
+            let restText = NSMutableAttributedString(string: "매월 첫째, 둘째 수요일 13:00 ~ 24:00\n공휴일, 일요일")
+            let restAttr = NSMutableAttributedString()
+            restAttr.append(restText)
+            restAttr.addAttributes([.paragraphStyle: style], range: NSRange(location: 0, length: restAttr.length))
+            cell.restLabel.attributedText = restAttr
+
+            cell.homeLabel.text = "공간 대여, 워크샵, 온라인 서점"
+
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: detailIdentifier2) as! detailCell2
