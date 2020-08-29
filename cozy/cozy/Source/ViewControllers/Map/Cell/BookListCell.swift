2186a733fc8ea2732ea0f574ede6e945dd75a0dd
Choieunji0114 <cheunji8209@naver.com>
2020-08-30 01:12:18 +0900

    update: MapTableView

--- cozy/cozy/Source/ViewControllers/Map/Cell/BookListCell.swift
+++ cozy/cozy/Source/ViewControllers/Map/Cell/BookListCell.swift
@@ -11,10 +11,25 @@ import UIKit
 class BookListCell: UITableViewCell {
 
     @IBOutlet weak var wholeView: UIView!
+    @IBOutlet weak var bookStoreImageView: UIImageView!
+
+    @IBOutlet weak var bookMarkButton: UIButton!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+
+    @IBOutlet weak var tag1: UIButton!
+    @IBOutlet weak var tag2: UIButton!
+
+    @IBOutlet weak var tag3: UIButton!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         wholeView.setViewShadow()
+
+        tag1.setMapTagButton()
+        tag2.setMapTagButton()
+        tag3.setMapTagButton()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
