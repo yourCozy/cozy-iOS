16ad34f7e598fe4f01fcbd951570e1a55b299cb1
jaeukyang <didwodnr123@gmail.com>
2020-08-31 16:33:27 +0900

    Update: (Display Recommend View) collectionView, constraints

---
+++ cozy/cozy/Source/ViewControllers/Recommend/Model/DisplayDetailData.swift
@@ -0,0 +1,18 @@
+//
+//  DisplayDetailData.swift
+//  cozy
+//
+//  Created by 양재욱 on 2020/08/31.
+//  Copyright © 2020 최은지. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct DisplayDetailData {
+    var detailImgName: String
+
+    init(detailImgName: String) {
+        self.detailImgName = detailImgName
+    }
+}
