481989d9951bf59dfb59a5008d7825187d8074bd
jaeukyang <didwodnr123@gmail.com>
2020-09-02 00:08:22 +0900

    Create: Onboarding View

---
+++ cozy/cozy/Source/ViewControllers/Onboarding/vc2.swift
@@ -0,0 +1,29 @@
+//
+//  vc2.swift
+//  cozy
+//
+//  Created by 양재욱 on 2020/09/01.
+//  Copyright © 2020 최은지. All rights reserved.
+//
+
+import UIKit
+
+class vc2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
