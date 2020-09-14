d5aa55c7bff565907a36edb75fbd232b3e7a15ac
jaeukyang <didwodnr123@gmail.com>
2020-09-15 01:41:10 +0900

    UPDATE: Comment Table View

---
+++ cozy/cozy/Source/API/ResponseModel/CommentModel.swift
@@ -0,0 +1,9 @@
+//
+//  CommentModel.swift
+//  cozy
+//
+//  Created by 양재욱 on 2020/09/15.
+//  Copyright © 2020 최은지. All rights reserved.
+//
+
+import Foundation
---
+++ cozy/cozy/Source/ViewControllers/Recommend/Cell/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  cozy
+//
+//  Created by 양재욱 on 2020/09/15.
+//  Copyright © 2020 최은지. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
