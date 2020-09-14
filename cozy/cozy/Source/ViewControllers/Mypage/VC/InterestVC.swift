f5600a52adb58c449d69402f45c79816a94c5730
Choieunji0114 <cheunji8209@naver.com>
2020-09-15 04:36:50 +0900

    fix: InterestVC

--- cozy/cozy/Source/ViewControllers/Mypage/VC/InterestVC.swift
+++ cozy/cozy/Source/ViewControllers/Mypage/VC/InterestVC.swift
@@ -10,47 +10,29 @@ import UIKit
 
 class InterestVC: UIViewController {
 
-    private let mapIdentifier2: String = "bookListCell"
-
+    private let interestIdentifier: String = "bookListCell"
     @IBOutlet weak var interestTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setInterestTableView()
+    }
 
-        // Do any additional setup after loading the view.
+    func setInterestTableView() {
         let nibName = UINib(nibName: "BookListCell", bundle: nil)
-        interestTableView.register(nibName, forCellReuseIdentifier: mapIdentifier2)
+        interestTableView.register(nibName, forCellReuseIdentifier: interestIdentifier)
         interestTableView.delegate = self
         interestTableView.dataSource = self
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
 extension InterestVC: UITableViewDelegate, UITableViewDataSource, UIViewControllerTransitioningDelegate {
 
-    // present half
-     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-         return HalfSizePresentationController(presentedViewController: presented, presenting: presenting)
-     }
-
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let sb = UIStoryboard(name: "BookDetail", bundle: nil)
         let vc = sb.instantiateViewController(identifier: "BookDetailVC") as! BookDetailVC
         self.navigationController?.pushViewController(vc, animated: true)
-    }
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -62,17 +44,17 @@ extension InterestVC: UITableViewDelegate, UITableViewDataSource, UIViewControll
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: mapIdentifier2) as! BookListCell
-            cell.selectionStyle = .none
+        let cell = tableView.dequeueReusableCell(withIdentifier: interestIdentifier) as! BookListCell
+        cell.selectionStyle = .none
 
-            cell.bookStoreImageView.image = UIImage(named: "asdfdghfgjhj")
-            cell.nameLabel.text = "코지서점"
-            cell.addressLabel.text = "서울특별시 용산구 한강대로 10길"
+        cell.bookStoreImageView.image = UIImage(named: "asdfdghfgjhj")
+        cell.nameLabel.text = "코지서점"
+        cell.addressLabel.text = "서울특별시 용산구 한강대로 10길"
 
-            cell.tag1.setTitle("    #베이커리    ", for: .normal)
-            cell.tag2.setTitle("    #심야책방    ", for: .normal)
-            cell.tag3.setTitle("    #맥주    ", for: .normal)
+        cell.tag1.setTitle("    #베이커리    ", for: .normal)
+        cell.tag2.setTitle("    #심야책방    ", for: .normal)
+        cell.tag3.setTitle("    #맥주    ", for: .normal)
 
-            return cell
+        return cell
     }
 }
