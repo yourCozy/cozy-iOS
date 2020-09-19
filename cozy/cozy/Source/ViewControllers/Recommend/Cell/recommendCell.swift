84442f7af1b5becf1ace01b566870d02346bbae2
Choieunji0114 <cheunji8209@naver.com>
2020-09-19 14:44:08 +0900

    fix: interestVC url

--- cozy/cozy/Source/ViewControllers/Recommend/Cell/recommendCell.swift
+++ cozy/cozy/Source/ViewControllers/Recommend/Cell/recommendCell.swift
@@ -18,8 +18,6 @@ class recommendCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
