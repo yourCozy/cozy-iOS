fde6c2043c11c730e351cb99ee3b3aebb455dd43
Choieunji0114 <cheunji8209@naver.com>
2020-10-04 17:22:07 +0900

    add: connect searchAPI

--- cozy/cozy/Source/ViewControllers/Search/VC/SearchListVC.swift
+++ cozy/cozy/Source/ViewControllers/Search/VC/SearchListVC.swift
@@ -25,12 +25,15 @@ class SearchListVC: UIViewController {
 
     var searchKeyword: String = "키워드"
 
+    private var searchList: [SearchData] = []
+
     private let searchListLabelIdentifier = "SearchLabelCell"
     private let searchListIdentifier: String = "SearchListCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        getData()
     }
 
     func setUI() {
@@ -41,6 +44,62 @@ class SearchListVC: UIViewController {
 
     @IBAction func goBack(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
+    }
+
+    func isKeyPresentInUserDefaults(key: String) -> Bool {
+        return UserDefaults.standard.object(forKey: key) != nil
+    }
+
+    func getData() {
+        if self.isKeyPresentInUserDefaults(key: "token") == true {
+            self.getSearchDataWithLogin()
+        } else {
+            self.getSearchData()
+        }
+    }
+
+    func getSearchDataWithLogin() {
+        SearchService.shared.getSearchListWithLogin(keyword: self.searchKeyword) { NetworkResult in
+            switch NetworkResult {
+            case.success(let data):
+                guard let data = data as? [SearchData] else { return }
+                self.searchList.removeAll()
+                for data in data {
+                    self.searchList.append(SearchData(bookstoreIdx: data.bookstoreIdx ?? 0, bookstoreName: data.bookstoreName ?? "", mainImg: data.mainImg ?? "", location: data.location ?? "", shortIntro1: data.shortIntro1 ?? "", shortIntro2: data.shortIntro2 ?? "", hashtag1: data.hashtag1 ?? "", hashtag2: data.hashtag2 ?? "", hashtag3: data.hashtag3 ?? "", checked: data.checked ?? 0, count: data.count ?? 0))
+                }
+                self.searchTableView.reloadData()
+            case .requestErr:
+                print("Request error")
+            case .pathErr:
+                print("path error")
+            case .serverErr:
+                print("server error")
+            case .networkFail:
+                print("network error")
+            }
+        }
+    }
+
+    func getSearchData() {
+        SearchService.shared.getSearchList(keyword: self.searchKeyword) { NetworkResult in
+            switch NetworkResult {
+            case.success(let data):
+                guard let data = data as? [SearchData] else { return }
+                self.searchList.removeAll()
+                for data in data {
+                    self.searchList.append(SearchData(bookstoreIdx: data.bookstoreIdx ?? 0, bookstoreName: data.bookstoreName ?? "", mainImg: data.mainImg ?? "", location: data.location ?? "", shortIntro1: data.shortIntro1 ?? "", shortIntro2: data.shortIntro2 ?? "", hashtag1: data.hashtag1 ?? "", hashtag2: data.hashtag2 ?? "", hashtag3: data.hashtag3 ?? "", checked: data.checked ?? 0, count: data.count ?? 0))
+                }
+                self.searchTableView.reloadData()
+            case .requestErr:
+                print("Request error")
+            case .pathErr:
+                print("path error")
+            case .serverErr:
+                print("server error")
+            case .networkFail:
+                print("network error")
+            }
+        }
     }
 }
 
@@ -54,7 +113,7 @@ extension SearchListVC: UITableViewDelegate, UITableViewDataSource {
         if section == 0 {
             return 1
         } else {
-            return 5
+            return self.searchList.count
         }
     }
 
@@ -63,7 +122,7 @@ extension SearchListVC: UITableViewDelegate, UITableViewDataSource {
             let cell = tableView.dequeueReusableCell(withIdentifier: self.searchListLabelIdentifier) as! SearchLabelCell
 
             let resultText1 = NSAttributedString(string: "검색 결과 ")
-            let resultText2 = NSAttributedString(string: "10", attributes: [.foregroundColor: UIColor.mango])
+            let resultText2 = NSAttributedString(string: "\(self.searchList.count)", attributes: [.foregroundColor: UIColor.mango])
             let resultText3 = NSAttributedString(string: "건")
 
             let attrString = NSMutableAttributedString()
